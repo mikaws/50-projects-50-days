6c980903c9d8731f4a89df2ee7a5dfda71f4a8fa
michael <michaelhenriqued@gmail.com>
2021-09-05 23:55:46 -0300

    call update function and fix querySelector

--- day-2-progress-steps/script.js
+++ day-2-progress-steps/script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-const circles = document.querySelectorAll('progress')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
@@ -9,8 +9,10 @@ next.addEventListener('click', () => {
     currentActive++
 
     if(currentActive > circles.length) {
-        currentActive = circle.length
+        currentActive = circles.length
     }
+    
+    update()
 })
 
 prev.addEventListener('click', () => {
@@ -19,11 +21,13 @@ prev.addEventListener('click', () => {
     if(currentActive < 1) {
         currentActive = 1
     }
+
+    update()
 })
 
 function update() {
-    circles.forEach((circle, i) => {
-        if(i < currentActive){
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive){
             circle.classList.add('active')
         } else {
             circle.classList.remove('active')
